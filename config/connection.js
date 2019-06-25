6f7812f1cdd88683a54976c071d768344e8dad13
NunyaBizness <jmclark801@yahoo.com>
2019-06-25 10:33:29 -0600

    Add connecton for JAWSDB

--- config/connection.js
+++ config/connection.js
@@ -2,23 +2,27 @@
 
 var mysql = require('mysql');
 require('dotenv').config();
+const connection;
 
-const connection = mysql.createConnection({
-  host: process.env.DB_HOST,
-  port: 3306,
-  password: process.env.DB_PASS,
-  user: process.env.DB_USER,
-  database: process.env.DB_DATABASE
-});
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection({
+    host: process.env.DB_HOST,
+    port: 3306,
+    password: process.env.DB_PASS,
+    user: process.env.DB_USER,
+    database: process.env.DB_DATABASE
+  });
+}
 
-connection.connect(function(err) {
+connection.connect(function (err) {
   if (err) {
     console.error('error connecting: ' + err.stack);
     return;
   }
- 
+
   console.log('connected as id ' + connection.threadId);
 });
 
 module.exports = connection;
-
